d19121c1a44f24b0cf60c191bae2accbf9d14358
raj <raj@gmail.com>
2023-07-12 17:56:43 +0530

    --- Auto Git Commit ---

---
+++ day3/main.tf
@@ -0,0 +1,31 @@
+Assignment 3
+provider "aws" {
+        alias = "NV"
+        region = "us-east-1"
+        access_key = ""
+        secret_key = ""
+}
+provider "aws" {
+        alias = "Ohio"
+        region = "us-east-2"
+        access_key = ""
+        secret_key = ""
+}
+resource "aws_instance" "assignment-3-1" {
+        provider = aws.NV
+        ami = "ami-08d4ac5b34553e16"
+        instance_type = "t2.micro"
+        key_name = "Sameer"
+        tags = {
+        Name = "hello-virginia"
+        }
+}
+resource "aws_instance" "assignment-3-2" {
+        provider = aws.Ohio
+        ami = "ami-0960ab670c8bb45f3"
+        instance_type = "t2.micro"
+        key_name = ""
+        tags = {
+        Name = "hello-ohio"
+        }
+}
