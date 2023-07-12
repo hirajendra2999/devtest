463bf5d25f1b2d60c65153b5d02b4775306ba486
raj <raj@gmail.com>
2023-07-12 17:17:45 +0530

    --- Auto Git Commit ---

--- day1/main.tf
+++ day1/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
         region = "us-east-2"
-        access_key = ""
-        secret_key = ""
+       # access_key = ""
+       # secret_key = ""
 }
 
 resource "aws_instance" "assignment-4" {
