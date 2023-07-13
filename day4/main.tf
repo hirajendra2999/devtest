ab579d63978e3e44d49c7e257920ddf6310a6f66
raj <raj@gmail.com>
2023-07-13 12:14:57 +0530

    --- Auto Git Commit ---

--- day4/main.tf
+++ day4/main.tf
@@ -23,7 +23,7 @@ resource "aws_vpc" "assignment-4-vpc" {
 
 resource "aws_subnet" "assignment-4-subnet" {
         vpc_id = aws_vpc.assignment-4-vpc.id
-        cidr_block = "10.10.1.0/1"
+        cidr_block = "10.10.1.0/20"
         availability_zone = "us-east-2a"
         tags = {
         Name = "assignment-4-subnet"
