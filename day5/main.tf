2b1e725410330378d095f086c52076f8df8d0d89
raj <raj@gmail.com>
2023-07-13 12:57:57 +0530

    --- Auto Git Commit ---

--- day5/main.tf
+++ day5/main.tf
@@ -20,12 +20,12 @@ output "IPv4" {
 
 
  install-apache2.sh 
- install-apache2.sh = []
+ install-apache2.sh = [
 #!/bin/bash
 sudo apt update -y
 sudo apt install apache2 -y
 sudo su
-echo "Custom html page" > /var/www/html/index.html
+echo "Custom html page" > /var/www/html/index.html ]
  
 
 /*user_data = <<-EOF
