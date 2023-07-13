ce50b4f8ca78a3146b076ee7b1707ec7c726cf83
raj <raj@gmail.com>
2023-07-13 12:29:32 +0530

    --- Auto Git Commit ---

---
+++ day5/main.tf
@@ -0,0 +1,28 @@
+provider "aws" {
+        #access_key = ""
+        #secret_key = ""
+        region = "us-east-2"
+}
+
+resource "aws_instance" "a5-instance" {
+        ami = "ami-0960ab670c8bb45f3"
+        instance_type = "t2.micro"
+        key_name = ""
+        user_data = "${file("install-apache2.sh")}"
+        tags = {
+                Name = "a5-instance"
+        }
+}
+
+output "IPv4" {
+        value = aws_instance.a5-instance.public_ip
+}
+
+
+install-apache2.sh
+install-apache2.sh
+#!/bin/bash
+sudo apt update -y
+sudo apt install apache2 -y
+sudo su
+echo "Custom html page" > /var/www/html/index.html
