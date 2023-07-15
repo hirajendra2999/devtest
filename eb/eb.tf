2208865a55e199e2bb35134c8b77ea2b4a04d3f9
raj <raj@gmail.com>
2023-07-15 14:53:45 +0530

    --- Auto Git Commit ---

--- eb/eb.tf
+++ eb/eb.tf
@@ -8,7 +8,7 @@ resource "aws_elastic_beanstalk_application" "example" {
 }
 
 resource "aws_elastic_beanstalk_environment" "example" {
-  name                = "example-env"  # Set your desired environment name
+  name                = "pqrst"  # Set your desired environment name
   application         = aws_elastic_beanstalk_application.example.name
   solution_stack_name = "64bit Amazon Linux 2 v3.5.3 running PHP 8.1"  # Set the desired solution stack for PHP 8.1
   # Alternatively, you can use `platform_arn` instead of `solution_stack_name` for more control over platform version.
