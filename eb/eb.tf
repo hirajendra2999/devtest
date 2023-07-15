9cac3a7fc48f275266febb062617bdf058ac2d31
raj <raj@gmail.com>
2023-07-15 14:33:38 +0530

    --- Auto Git Commit ---

--- eb/eb.tf
+++ eb/eb.tf
@@ -9,7 +9,7 @@ resource "aws_elastic_beanstalk_application" "tftest" {
 
 resource "aws_elastic_beanstalk_environment" "example" {
   name                = "example-env"  # Set your desired environment name
-  application         = aws_elastic_beanstalk_application.example.name
+  application         = aws_elastic_beanstalk_application.tftest.name
   solution_stack_name = "64bit Amazon Linux 2 v3.5.3 running PHP 8.1"  # Set the desired solution stack for PHP 8.1
   # Alternatively, you can use `platform_arn` instead of `solution_stack_name` for more control over platform version.
 
