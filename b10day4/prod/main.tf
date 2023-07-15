377f21b196ca70b433b8ed9546792d9ea5bea31d
raj <raj@gmail.com>
2023-07-15 07:03:12 +0530

    --- Auto Git Commit ---

--- b10day4/prod/main.tf
+++ b10day4/prod/main.tf
@@ -1,6 +1,7 @@
 module "myvpc" {
     source = "../modules/vpc"
-    vpc_cidr = "${var.vpc_cidr}"
+    #vpc_cidr = "${var.vpc_cidr}"
+    
     env_tag = "production"
     vpc_id = "${module.myvpc.vpcid}"
     subnet_cidr = "${var.subnet_cidr}"
