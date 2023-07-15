016a1378294dae3097efbef5524c88142f21d56d
raj <raj@gmail.com>
2023-07-15 13:41:57 +0530

    --- Auto Git Commit ---

---
+++ eb/eb.tf
@@ -0,0 +1,73 @@
+provider "aws" {
+  region = "ap-south-1"  # Set your desired region
+}
+
+resource "aws_elastic_beanstalk_application" "test" {
+  name        = "test-app"  # Set your desired application name
+  description = "test Elastic Beanstalk Application"
+}
+
+resource "aws_elastic_beanstalk_environment" "abcd" {
+  name                = "abcd"  # Set your desired environment name
+  application         = aws_elastic_beanstalk_application.test-app.name
+  solution_stack_name = "64bit Amazon Linux 2 v5.4.3 running PHP 8.1"  # Set the desired solution stack for PHP 8.1
+  # Alternatively, you can use `platform_arn` instead of `solution_stack_name` for more control over platform version.
+
+  # Custom configuration
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "EnvironmentType"
+    value     = "Custom configuration"  # Use Single Instance environment type
+  }
+
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "KeyName"
+    value     = "rajmumbai"  # Set the name of your EC2 key pair
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-0e71241ea27b82753"  # Set the ID of your desired VPC
+  }
+
+  # Instance settings
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "InstanceType"
+    value     = "t2.micro"  # Set the desired instance type
+  }
+
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "EnableDedicatedTenancy"
+    value     = "false"  # Set to true if you require dedicated tenancy
+  }
+
+  setting {
+    namespace = "aws:ec2:instances"
+    name      = "ProxyServer"
+    value     = "Apache"  # Set the proxy server details
+  }
+
+  # Load balancer settings
+  setting {
+    namespace = "aws:elasticbeanstalk:environment"
+    name      = "LoadBalancerType"
+    value     = "application"  # Use an Application Load Balancer
+  }
+
+  # Availability Zone selection
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "Availability Zones"
+    value     = "ap-south-1a, ap-south-1b"  # Set the desired availability zones
+  }
+
+  # Additional manual configuration settings can be added as needed
+
+  tags = {
+    Name = "example-env"  # Set your desired tags
+  }
+}
