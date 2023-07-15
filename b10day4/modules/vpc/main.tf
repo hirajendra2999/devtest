f5adc27e8b300e82c3d4ded2256290ed99c68eb0
raj <raj@gmail.com>
2023-07-15 06:47:52 +0530

    --- Auto Git Commit ---

--- b10day4/modules/vpc/main.tf
+++ b10day4/modules/vpc/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "my-vpc" {
     cidr_block = "${var.vpc_cidr}"
     tags  = {
         Name = "my-vpc"
-        Env = "${var.env.tag}"
+        Env = "${var.env_tag}"
     }  
 }
 resource "aws_subnet" "public-subnet-a" {
@@ -10,7 +10,7 @@ resource "aws_subnet" "public-subnet-a" {
   cidr_block              = "${var.subnet_cidr}"
    tags                   = { 
     Name                  =  "public_subnet"
-    Env                   = "${var.env.tag}"
+    Env                   = "${var.env_tag}"
   }
   map_public_ip_on_launch = "${var.allow_public_ip}"
   availability_zone        = "${var.az_name}"
