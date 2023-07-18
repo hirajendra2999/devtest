52086d357d1702cbc770c58d4afd71d51292cb8d
raj <raj@gmail.com>
2023-07-18 13:14:42 +0530

    --- Auto Git Commit ---

--- eb/eb.tf
+++ eb/eb.tf
@@ -9,7 +9,7 @@ resource "aws_elastic_beanstalk_application" "tftest" {
   description = "abcd Elastic Beanstalk Application"
 }
 
-resource "aws_elastic_beanstalk_environment" "example" {
+/*resource "aws_elastic_beanstalk_environment" "example" {
   name                = "pqrst"  # Set your desired environment name
   application         = aws_elastic_beanstalk_application.abcd.name  
   
