fd1f7c9bfea0da0bf40446fb36309abbbfc376a8
raj <raj@gmail.com>
2023-07-23 08:26:12 +0530

    --- Auto Git Commit ---

---
+++ jenkin/old pl/pipelines/abc.groovy
@@ -0,0 +1,25 @@
+pipeline {
+    agent any 
+    stages {
+        stage('Pull') { 
+            steps {
+                git branch: 'main', credentialsId: 'rj', url: 'https://github.com/hirajendra2999/devtest.git'
+            }
+        }
+        stage('Build') { 
+            steps {
+                sh 'echo "Build successefull"'
+            }    
+        }
+        stage('Test') { 
+            steps {
+                echo 'Test Success!'
+            }    
+        }
+        stage('Deploy') { 
+            steps {
+                sh 'echo "Deploy successefull"'
+            }    
+        }
+    }
+}    
