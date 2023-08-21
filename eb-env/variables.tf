662372af6744f04e621fd41553a3578131d61b61
raj <raj@gmail.com>
2023-08-21 16:01:12 +0530

    --- Auto Git Commit ---

--- eb-env/variables.tf
+++ eb-env/variables.tf
@@ -202,7 +202,7 @@ variable "as_upper_threshold" {
 # Namespace: aws:elasticbeanstalk:container:php:phpini
 variable "eb_solution_stack_name" {
   # type    = "string"
-  default = "64bit Amazon Linux 2 v3.5.9 running PHP 8.1"
+  default = "64bit Amazon Linux 2 v3.5.9"
   description = "The Elastic Beanstalk solution stack name"
 }
 variable "php_version" {
